202cb11e7a193bd359ec5ed5596a2620a91611ff
li zi <li@entangled.one>
2021-11-13 17:29:18 -0800

    add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
+import vercel from '@sveltejs/adapter-vercel'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: 'body',
-		router: false
+		router: false,
+		adapter: vercel()
 	}
 };
 
