325f0dba38bf2849d40199d4c1ffcf320efc95b4
li zi <li@entangled.one>
2022-01-02 12:28:52 -0800

    change query to searchParams

--- src/routes/tips/history.json.js
+++ src/routes/tips/history.json.js
@@ -1,9 +1,10 @@
 import { getAllDocuments } from '$lib/db'
 
-export async function get({ query }) {
+export async function get({ url }) {
+  const { searchParams } = url
   const docs = await getAllDocuments({
-    queryBefore: query.get('before'),
-    queryAfter: query.get('after'),
+    queryBefore: searchParams.get('before'),
+    queryAfter: searchParams.get('after'),
   })
   return {
     body: docs
