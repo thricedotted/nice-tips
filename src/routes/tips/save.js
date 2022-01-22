8ded498f17ec7c61e2247b140da46aef489994a6
li zi <li@entangled.one>
2022-01-21 16:38:40 -0800

    update for new sveltekit Request object
    https://github.com/sveltejs/kit/pull/3384

--- src/routes/tips/save.js
+++ src/routes/tips/save.js
@@ -1,8 +1,10 @@
 import { createDocument } from '$lib/db'
 import { verifyHash } from '$lib/crypto'
 
-export async function post({ body }) {
-  const { text, url, hash } = Object.fromEntries(body)
+export async function post({ request }) {
+  const formData = await request.formData()
+
+  const { text, url, hash } = Object.fromEntries(formData)
 
   if (verifyHash(text, url, hash)) {
     const { ref } = await createDocument({ 
