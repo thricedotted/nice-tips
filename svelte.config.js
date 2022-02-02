f66a0801808f90aeaab6dc0eeb1bca6a915dbf12
li zi <li@entangled.one>
2022-02-01 16:10:42 -0800

    add previous months to prerender (untested)

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,16 @@ import vercel from '@sveltejs/adapter-vercel'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
 		target: 'body',
-
-		// that's right! no JS!!! smiling imp emoji!!!
-		// router: false,
-		// hydrate: false,
-
-		adapter: vercel()
+		adapter: vercel(),
+		prerender: {
+			entries: [
+				// TODO: automatically generate prerender entries
+				'/archive/2021/11',
+				'/archive/2021/12',
+				'/archive/2022/01'
+			]
+		},
 	}
 };
 
