cd6c7d80818d3f38260eb83ca0d65ce4b9bf85f7
li zi <li@entangled.one>
2022-05-26 14:12:23 -0700

    update sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ import vercel from '@sveltejs/adapter-vercel'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		target: 'body',
 		adapter: vercel(),
 		prerender: {
 			entries: [
