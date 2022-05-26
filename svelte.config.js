fef08aecfd4a96a80f748e718b6a7591167f5f8a
li zi <li@entangled.one>
2022-05-26 14:22:44 -0700

    add latest months to prerender

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,13 @@ const config = {
 		adapter: vercel(),
 		prerender: {
 			entries: [
-				// TODO: automatically generate prerender entries
+				// TODO: automatically generate prerender months past
 				'/archive/2021/11',
 				'/archive/2021/12',
-				'/archive/2022/01'
+				'/archive/2022/01',
+				'/archive/2022/02',
+				'/archive/2022/03',
+				'/archive/2022/04'
 			]
 		},
 	}
